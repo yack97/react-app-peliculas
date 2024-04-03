037325805ec764a16597d0ebb65dc89a8113716d
yack97 <yap30600@gmail.com>
2024-04-03 20:23:13 -0300

    practica react app peliculas

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { MovieApp } from './MovieApp.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+
+    <MovieApp/>
+  </React.StrictMode>,
+)
